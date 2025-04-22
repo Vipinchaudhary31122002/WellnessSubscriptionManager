1c0b4b3e9d120e12505854e8cd33f1d1d7e97fb5
Vipin Chaudhary <vipinchaudhary31122002@gmail.com>
2025-04-22 12:32:31 +0530

    added mongodb urls
    Signed-off-by: Vipin Chaudhary <vipinchaudhary31122002@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vitalcore')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -55,5 +55,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 }); 
